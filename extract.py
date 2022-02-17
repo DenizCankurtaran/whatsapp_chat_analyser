cda4751d7414f173cb3a2deaf882239818242e31
Deniz Cankurtaran <deniz.cankurtaran@hotmail.de>
2022-02-17 22:21:15 +0100

    initial commit

---
+++ extract.py
@@ -0,0 +1,57 @@
+from asyncore import write
+import csv
+
+def get_sentences():
+	with open('WhatsAppChat.txt', 'r', encoding='utf-8') as f:
+		data = f.readlines()
+
+	cleaned_data = []
+	prev = []
+
+	for line in data:
+		try:
+			isDate = False
+			int(line[0])
+			if line[1] == '/' or line[2] == '/':
+				isDate = True
+			if not isDate:
+				raise Exception
+			
+			if prev != []:
+				cleaned_data.append(' '.join(prev))
+				prev = []
+				prev.append(line)
+			else:
+				prev.append(line)
+
+		except Exception as e:
+			prev.append(line)
+		
+	cleaned_data.append(' '.join(prev))
+	return cleaned_data
+
+def write_csv(cleaned_data):
+	bad_sentences = ['Messages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them. Tap to learn more.', '<Media omitted>']
+	with open('chat.csv', 'w', encoding='utf-8') as chat_f:
+		writer = csv.writer(chat_f)
+		# writer.writerow(['author', 'date', 'message'])
+		for line in cleaned_data:
+			
+			isBad = False
+			for bad_sentence in bad_sentences:
+				if bad_sentence in line:
+					isBad = True
+			if isBad:
+				continue
+
+			m_date = line.split(', ')[0]
+
+			start = line.find("- ") + len('- ')
+			end = line.find(": ")
+			author = line[start:end]
+			clean_line = ' '.join(line.split(': ')[1:]).replace('\n', ' ').replace(',', ' ')
+			writer.writerow([author, m_date, clean_line])
+
+
+cleaned_data = get_sentences()
+write_csv(cleaned_data)
---
+++ main.py
@@ -0,0 +1,79 @@
+import csv
+import emoji as emoji_lib
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.font_manager as fm
+import os
+
+
+def change_matplotlib_font(font_download_url):
+    FONT_PATH = 'MY_FONT'
+    
+    font_download_cmd = f"wget {font_download_url} -O {FONT_PATH}.zip"
+    unzip_cmd = f"unzip -o {FONT_PATH}.zip -d {FONT_PATH}"
+    os.system(font_download_cmd)
+    os.system(unzip_cmd)
+    
+    font_files = fm.findSystemFonts(fontpaths=FONT_PATH)
+    for font_file in font_files:
+        fm.fontManager.addfont(font_file)
+
+    font_name = fm.FontProperties(fname=font_files[0]).get_name()
+    matplotlib.rc('font', family=font_name)
+    print("font family: ", plt.rcParams['font.family'])
+
+def extract_emojis(s):
+  return [c for c in s if c in emoji_lib.UNICODE_EMOJI['en']]
+
+def get_stats():
+	with open('chat.csv', 'r', encoding='utf-8') as file:
+		chars = 'abcdefghijklmnopqr'
+		lines = [line for line in csv.reader(file) if line != []]
+		stats = {}
+		for line in lines:
+			author, date, message = line[0], line[1], line[2]
+			emojis = extract_emojis(message)
+			emoji_set = set(emojis)
+			if not date in stats:
+				stats[date] = {}
+			for emoji in emojis:
+				if emoji in stats[date]:
+					stats[date][emoji] += 1
+				else:
+					stats[date][emoji] = 1
+		return stats
+
+def make_bar_chart(emoji, stats):
+	labels = [date for date in stats.keys()]
+
+	x = np.arange(len(labels))  # the label locations
+
+	fig, ax = plt.subplots()
+	ax.set_label(emoji)	
+	for label in labels:
+		if emoji in stats[label]:
+			count = stats[label][emoji]
+			rect = ax.bar(label, count)
+		else:
+			count = 0
+			rect = ax.bar(label, count)
+		ax.bar_label(rect, padding=3)
+		
+	ax.set_ylabel('Number')
+	ax.set_title(emoji)
+	ax.set_xticks(x, labels)
+	fig.tight_layout()
+
+	plt.show()
+
+def make_charts(stats):
+	emojis = ['💩', '🔪', '🤣', '👍', '🥰', '🙌', '😂']
+	for emoji in emojis:
+		make_bar_chart(emoji, stats)
+
+stats = get_stats()
+font_download_url = "https://fonts.google.com/download?family=Noto%20Sans"
+change_matplotlib_font(font_download_url)
+# print(stats['2/17/22']['💩'])
+make_charts(stats)
